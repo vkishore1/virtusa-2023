43244421668e3b3be5b71c15592537e1b12adac3
vkishore1 <91901531+vkishore1@users.noreply.github.com>
2022-11-28 10:34:04 +0530

    Update factorial.java

--- java/factorial.java
+++ java/factorial.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter a number-");
 		int a=sc.nextInt();
